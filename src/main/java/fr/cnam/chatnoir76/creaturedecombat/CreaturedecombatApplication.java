e7cd758c3434baef3da559750015ce9469163e51
ChatNoir76 <chatnoirvip@gmail.com>
2024-03-24 11:16:41 +0100

    feat():update gradle settings

---
+++ src/main/java/fr/cnam/chatnoir76/creaturedecombat/CreaturedecombatApplication.java
@@ -0,0 +1,13 @@
+package fr.cnam.chatnoir76.creaturedecombat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreaturedecombatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreaturedecombatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/cnam/chatnoir76/creaturedecombat/CreaturedecombatApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cnam.chatnoir76.creaturedecombat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreaturedecombatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
